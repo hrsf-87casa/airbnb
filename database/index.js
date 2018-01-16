0a7d339d0034dd4f6566e32610375fcfbd0176a5
Austin Shin <shinaustin@gmail.com>
2018-01-16 15:07:37 -0800

    rebase starting point

--- database/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  getAllListings: require('./queries/getAllListings.js'),
-  user: require('./queries/getBooking.js'),
-  getListingsByCity: require('./queries/getListingsByCity.js'),
-  checkAvailability: require('./queries/checkAvailability.js'),
-  saveReservation: require('./queries/saveReservation.js'),
-  getListingById: require('./queries/getListingById.js'),
-};
