591e9516f54d00af015d64d6238b7ec12429e705
Andy Dam <andy@andydam.com>
2018-01-17 11:40:08 -0800

    Update config.js

--- database/config.js
+++ database/config.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection(process.env.DATABASE_UR);
+const connection = mysql.createConnection(process.env.DATABASE_URL);
 
 connection.connect((err) => {
   if (err) {
