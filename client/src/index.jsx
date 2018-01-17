9078fd4bd251ff3dcd6e2048b6c97cf965cab5ce
Shawn Ranck <30805656+smranck@users.noreply.github.com>
2018-01-16 20:21:10 -0800

    (bug) Delete Merge Conflict

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,88 +7,15 @@ import Main from './components/Main.jsx';
 import UserComponent from './components/UserComponent.jsx';
 import ListingEntryDetails from './components/ListingEntryDetails.jsx';
 import Navigation from './components/Navigation.jsx';
-<<<<<<< HEAD
 import App from './components/App.jsx';
 import Login from './components/Login.jsx';
 import Signup from './components/Signup.jsx';
-=======
-import Signup from './components/Signup.jsx';
-import Login from './components/Login.jsx';
-
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      listings: [],
-      bookings: [],
-      city: null,
-      searched: false,
-    };
-    this.search = this.search.bind(this);
-    this.goHome = this.goHome.bind(this);
-    this.goToTrips = this.goToTrips.bind(this);
-  }
-
-  search(city) {
-    axios
-      .get('/listings-bryce', {
-        params: {
-          city: city,
-        },
-      })
-      .then((response) => {
-        this.setState({ listings: response.data, city: city, searched: true });
-        this.forceUpdate();
-        this.props.history.push('/listings/' + city);
-        this.props.location.state = { city: city };
-      })
-      .catch((error) => console.log(error));
-  }
-
-  goHome() {
-    this.setState({ listings: [], searched: false });
-  }
-
-  goToTrips() {
-    this.setState({ searched: true });
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="nav">
-          <Navigation
-            searched={this.state.searched}
-            search={this.search}
-            goHome={this.goHome}
-            goToTrips={this.goToTrips}
-          />
-        </div>
-        {this.state.searched ? null : (
-          <div className="mainSearch">
-            <Search search={this.search} />
-          </div>
-        )}
-      </div>
-    );
-  }
-}
->>>>>>> (refactor) refactor for consistent naming
 
 ReactDOM.render(
   <BrowserRouter history={BrowserHistory}>
     <div>
-<<<<<<< HEAD
-<<<<<<< HEAD
       <Route exact path="/" component={App} />
       <Route path="/" component={Navigation} />
-=======
-      <Route path="/" component={Login} />
->>>>>>> (refactor) refactor for consistent naming
-=======
-      <Route path="/" component={App} />
->>>>>>> (feat) Create basic login and signup components
       <Route path="/bookings" component={UserComponent} />
       <Route exact path="/listings/:state--:city" component={Listings} />
       <Route
