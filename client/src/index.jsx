417c8a905c176a31f838f4e1afd52cbb027e4a7f
Shawn Ranck <30805656+smranck@users.noreply.github.com>
2018-01-17 20:14:33 -0800

    (refactor) Merge conflict

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-<<<<<<< HEAD
-=======
-import Listings from './components/Listings.jsx';
->>>>>>> (Refactor) Merge conflicts
 import { BrowserRouter, Route, BrowserHistory, Link } from 'react-router-dom';
 import UserComponent from './components/UserComponent.jsx';
 import ListingEntryDetails from './components/ListingEntryDetails.jsx';
@@ -13,10 +9,6 @@ import App from './components/App.jsx';
 import Login from './components/Login.jsx';
 import Signup from './components/Signup.jsx';
 import Main from './components/Main.jsx';
-<<<<<<< HEAD
-=======
-import AddListing from './components/AddListing.jsx';
->>>>>>> (Refactor) Merge conflicts
 
 ReactDOM.render(
   <BrowserRouter history={BrowserHistory}>
@@ -26,11 +18,7 @@ ReactDOM.render(
       <Route exact path="/signup" component={Signup} />
       <Route exact path="/login" component={Login} />
       <Route path="/bookings" component={UserComponent} />
-<<<<<<< HEAD
       <Route exact path="/listings/:state--:city" component={Results} />
-=======
-      <Route exact path="/listings/:state--:city" component={Listings} />
->>>>>>> (Refactor) Merge conflicts
       <Route path="/listings/:state--:city/:id" component={ListingEntryDetails} />
     </div>
   </BrowserRouter>,
