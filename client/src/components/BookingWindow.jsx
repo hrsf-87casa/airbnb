b285d322e2c3a1c3699b9020cb751ddaa1817401
Andy Dam <andy@andydam.com>
2018-01-17 18:51:15 -0800

    (refactor) remove jquery

--- client/src/components/BookingWindow.jsx
+++ client/src/components/BookingWindow.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
 import axios from 'axios';
 import Modal from 'react-responsive-modal';
 import { emojify } from 'react-emojione';
