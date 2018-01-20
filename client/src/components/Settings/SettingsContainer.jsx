16fb74f2d95ed8dceda1af22c39d9309d876e786
Austin Shin <shinaustin@gmail.com>
2018-01-20 15:18:56 -0800

    (feat/userinfo) create settings container which functions as tabs

--- client/src/components/Settings/SettingsContainer.jsx
+++ client/src/components/Settings/SettingsContainer.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
 import {
   Container,
   Row,
@@ -11,48 +10,48 @@ import {
   Form,
   FormGroup,
   Input,
-} from 'reactstrap'
-import renderIf from 'render-if'
+} from 'reactstrap';
+import renderIf from 'render-if';
 import ProfileTab from './ProfileTab.jsx';
 import PhotoUploadTab from './PhotoUploadTab.jsx';
 import ResetPasswordTab from './ResetPasswordTab.jsx';
 
 export default class SettingsContainer extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      currentTab: 'profile'
-    }
+      currentTab: 'profile',
+    };
   }
   toggleProfileTab() {
-    this.setState({ currentTab: 'profile' })
+    this.setState({ currentTab: 'profile' });
   }
   togglePhotosTab() {
-    this.setState({ currentTab: 'photos' })
+    this.setState({ currentTab: 'photos' });
   }
   toggleResetTab() {
-    this.setState({ currentTab: 'reset' })
+    this.setState({ currentTab: 'reset' });
   }
 
-  render () {
-    const { currentTab } = this.state
-    const profileTab = renderIf(currentTab === 'profile')
-    const photosTab = renderIf(currentTab === 'photos')
-    const resetTab = renderIf(currentTab === 'reset')
+  render() {
+    const { currentTab } = this.state;
+    const profileTab = renderIf(currentTab === 'profile');
+    const photosTab = renderIf(currentTab === 'photos');
+    const resetTab = renderIf(currentTab === 'reset');
     const styles = {
       container: {
         backgroundColor: 'white',
-        height: '100vh'
+        height: '100vh',
       },
       sidebar: {
         backgroundColor: 'white',
-        height: '100vh'
+        height: '100vh',
       },
       content: {
         backgroundColor: '#edefed',
-        height: '100vh'
+        height: '100vh',
       },
-    }
+    };
     return (
       <Container style={styles.container}>
         <Row>
@@ -76,40 +75,25 @@ export default class SettingsContainer extends React.Component {
                 </NavItem>
                 <NavItem>
                   <NavLink href="/profile">
-                    <Button outline color="primary">View Profile</Button>
+                    <Button outline color="primary">
+                      View Profile
+                    </Button>
                   </NavLink>
                 </NavItem>
               </Nav>
             </Container>
           </Col>
           <Col xs="9" className="content" style={styles.content}>
-            {profileTab(
-              <ProfileTab />
-            )}
-            {photosTab(
-              <PhotoUploadTab />
-            )}
-            {resetTab(
-              <ResetPasswordTab />
-            )}
+            {profileTab(<ProfileTab />)}
+            {photosTab(<PhotoUploadTab />)}
+            {resetTab(<ResetPasswordTab />)}
           </Col>
         </Row>
       </Container>
-    )
+    );
   }
 }
 
 // option to upload a file to database as profile picture
 // option to set displayname if you choose which will render on the nav bar
 
-/*
-SettingsContainer.propTypes = {
-  optionalArray: PropTypes.array,
-  optionalBool: PropTypes.bool,
-  optionalFunc: PropTypes.func,
-  optionalNumber: PropTypes.number,
-  optionalObject: PropTypes.object,
-  optionalString: PropTypes.string,
-  optionalSymbol: PropTypes.symbol,
-};
-*/
