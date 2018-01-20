7a1b35451bc2905a0be399591ecb033c44e5a724
Austin Shin <shinaustin@gmail.com>
2018-01-20 15:18:19 -0800

    (feat/userinfo) implement database functions to handle queries

--- database/queries/users.js
+++ database/queries/users.js
@@ -1,7 +1,9 @@
 const connection = require('../config');
 
 const get = username =>
-  connection.queryAsync('SELECT * FROM users WHERE name = ?', [username]).then(data => data[0]);
+  connection
+    .queryAsync('SELECT * FROM users WHERE name = ?', [username])
+    .then(data => data[0]);
 
 const create = (username, password, phoneNumber, email) =>
   connection.queryAsync(
@@ -10,14 +12,34 @@ const create = (username, password, phoneNumber, email) =>
   );
 
 const getById = id =>
-  connection.queryAsync('SELECT * FROM users WHERE id = ?', [id]).then(data => data[0]);
+  connection
+    .queryAsync('SELECT * FROM users WHERE id = ?', [id])
+    .then(data => data[0]);
 
 const getAllUserInfo = id =>
-  connection.queryAsync('SELECT * FROM users WHERE id = ?', [id]).then(data => data[0]);
+  connection
+    .queryAsync('SELECT * FROM users WHERE id = ?', [id])
+    .then(data => data[0]);
+
+const uploadPicture = (id, profilePicture) => {
+  connection.queryAsync('UPDATE users SET picture = ? WHERE id = ?', [
+    profilePicture,
+    id,
+  ]);
+};
+
+const updateProfile = (id, displayName, location, tagline, bio) => {
+  connection.queryAsync(
+    'UPDATE users SET displayName = ?, location = ?, tagline =?, bio = ? WHERE id = ?',
+    [displayName, location, tagline, bio, id],
+  );
+};
 
 module.exports = {
   get,
   create,
   getById,
   getAllUserInfo,
+  uploadPicture,
+  updateProfile,
 };
