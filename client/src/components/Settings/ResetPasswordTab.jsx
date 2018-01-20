9e447a68f8d8e7adebbb2ce3adbe6a9aaca30c25
Austin Shin <shinaustin@gmail.com>
2018-01-20 15:19:22 -0800

    (feat/userinfo) implement ResetPasswordTab component non-functional atm

--- client/src/components/Settings/ResetPasswordTab.jsx
+++ client/src/components/Settings/ResetPasswordTab.jsx
@@ -1,38 +1,30 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {
-  Form,
-  FormGroup,
-  Input,
-  Label,
-  Row,
-  Col,
-} from 'reactstrap';
+import React from 'react';
+import { Form, FormGroup, Input, Label, Row, Col } from 'reactstrap';
 
 export default class ResetPasswordTab extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {}
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
 
-  render () {
+  render() {
     return (
       <Form>
         <br />
         <FormGroup>
           <Row>
-            <Col xs='3'>
-              <Label for='oldPassword' className='tabLabel'>
+            <Col xs="3">
+              <Label for="oldPassword" className="tabLabel">
                 {' '}
                 Old Password{' '}
               </Label>
             </Col>
-            <Col xs='9'>
+            <Col xs="9">
               <Input
-                type='password'
-                name='oldPassword'
-                id='oldPassword'
-                placeholder='Enter old password here.'
+                type="password"
+                name="oldPassword"
+                id="oldPassword"
+                placeholder="Enter old password here."
               />
             </Col>
           </Row>
@@ -40,33 +32,23 @@ export default class ResetPasswordTab extends React.Component {
 
         <FormGroup>
           <Row>
-            <Col xs='3'>
-              <Label for='newPassword' className='tabLabel'>
+            <Col xs="3">
+              <Label for="newPassword" className="tabLabel">
                 New Password
               </Label>
             </Col>
-            <Col xs='9'>
+            <Col xs="9">
               <Input
-                type='password'
-                name='newPassword'
-                id='newPassword'
-                placeholder='Enter new password here'
+                type="password"
+                name="newPassword"
+                id="newPassword"
+                placeholder="Enter new password here"
               />
             </Col>
           </Row>
         </FormGroup>
-        <button className='todo'>Reset</button>
+        <button className="todo">Reset</button>
       </Form>
-    )
+    );
   }
-}
-
-ResetPasswordTab.propTypes = {
-  optionalArray: PropTypes.array,
-  optionalBool: PropTypes.bool,
-  optionalFunc: PropTypes.func,
-  optionalNumber: PropTypes.number,
-  optionalObject: PropTypes.object,
-  optionalString: PropTypes.string,
-  optionalSymbol: PropTypes.symbol
 }
