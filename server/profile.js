e3b296fd585d2b3127740d53ffb185fb17955955
Austin Shin <shinaustin@gmail.com>
2018-01-20 15:17:22 -0800

    (feat/userinfo) implement backend route handling for settings and profile

--- server/profile.js
+++ server/profile.js
@@ -1,6 +1,10 @@
 const { users } = require('../database');
 
 const getAllUserInfo = userId => users.getAllUserInfo(userId);
+const uploadPicture = (userId, profilePicture) =>
+  users.uploadPicture(userId, profilePicture);
 
+const updateProfile = (userId, displayName, location, tagline, bio) =>
+  users.updateProfile(userId, displayName, location, tagline, bio);
 
-module.exports = { getAllUserInfo };
+module.exports = { getAllUserInfo, uploadPicture, updateProfile };
--- server/routers.js
+++ server/routers.js
@@ -12,19 +12,27 @@ const bookings = require('./bookings');
 const listings = require('./listings');
 const profile = require('./profile');
 
+const multer = require('multer'),
+  multerS3 = require('multer-s3'),
+  fs = require('fs'),
+  AWS = require('aws-sdk');
+
 const router = express.Router();
-const reactRoute = (req, res) => res.sendFile(path.resolve(__dirname, '../client/dist/index.html'));
+const reactRoute = (req, res) =>
+  res.sendFile(path.resolve(__dirname, '../client/dist/index.html'));
 const protectedReactRoute = (req, res) =>
-  (req.session.passport
+  req.session.passport
     ? res.sendFile(path.resolve(__dirname, '../client/dist/index.html'))
-    : res.redirect('/login'));
+    : res.redirect('/login');
 
 /*
   Passport and user authentication
 */
 
 router.use(cookieParser());
-router.use(session({ secret: 'airbnb-casa', resave: false, saveUninitialized: false }));
+router.use(
+  session({ secret: 'airbnb-casa', resave: false, saveUninitialized: false }),
+);
 router.use(passport.initialize());
 router.use(passport.session());
 
@@ -34,7 +42,12 @@ router.post('/signup', async (req, res) => {
       return res.sendStatus(409);
     }
 
-    await auth.addUser(req.body.username, req.body.password, req.body.phoneNumber, req.body.email);
+    await auth.addUser(
+      req.body.username,
+      req.body.password,
+      req.body.phoneNumber,
+      req.body.email,
+    );
     return res.sendStatus(200);
   } catch (err) {
     return res.status(500).json(err.stack);
@@ -45,7 +58,8 @@ router.post('/login', passport.authenticate('local'), (req, res) =>
   res
     .set('access-control-allow-credentials', 'true')
     .status(200)
-    .json({ userId: req.session.passport.user }));
+    .json({ userId: req.session.passport.user }),
+);
 
 /*
   Listings search and details
@@ -53,7 +67,9 @@ router.post('/login', passport.authenticate('local'), (req, res) =>
 
 router.post('/api/listings/search', async (req, res) => {
   try {
-    return res.status(200).json(await airbnb.search(req.body.city, req.body.state));
+    return res
+      .status(200)
+      .json(await airbnb.search(req.body.city, req.body.state));
   } catch (err) {
     return res.status(500).json(err.stack);
   }
@@ -91,12 +107,14 @@ router.post('/api/bookings/reserve', async (req, res) => {
 
     return res
       .status(200)
-      .json(await bookings.reserve(
-        req.session.passport.user,
-        req.body.listingId,
-        req.body.start,
-        req.body.end,
-      ));
+      .json(
+        await bookings.reserve(
+          req.session.passport.user,
+          req.body.listingId,
+          req.body.start,
+          req.body.end,
+        ),
+      );
   } catch (err) {
     return res.status(500).json(err.stack);
   }
@@ -108,7 +126,9 @@ router.post('/api/bookings/cancel', async (req, res) => {
       return res.sendStatus(401);
     }
 
-    return res.sendStatus(await bookings.cancel(req.body.bookingId, req.session.passport.user));
+    return res.sendStatus(
+      await bookings.cancel(req.body.bookingId, req.session.passport.user),
+    );
   } catch (err) {
     return res.status(500).json(err.stack);
   }
@@ -136,7 +156,9 @@ router.post('/api/listings/host', async (req, res) => {
       return res.sendStatus(401);
     }
 
-    return res.status(200).json(await listings.host(req.body, req.session.passport.user));
+    return res
+      .status(200)
+      .json(await listings.host(req.body, req.session.passport.user));
   } catch (err) {
     return res.status(500).json(err.stack);
   }
@@ -148,7 +170,9 @@ router.post('/api/listings/cancel', async (req, res) => {
       return res.sendStatus(401);
     }
 
-    return res.sendStatus(await listings.cancel(req.body.listingId, req.session.passport.user));
+    return res.sendStatus(
+      await listings.cancel(req.body.listingId, req.session.passport.user),
+    );
   } catch (err) {
     return res.status(500).json(err.stack);
   }
@@ -159,12 +183,69 @@ router.get('/api/user/profile', async (req, res) => {
     if (!req.session.passport) {
       return res.sendStatus(401);
     }
-    return res.status(200).json(await profile.getAllUserInfo(req.session.passport.user));
+    return res
+      .status(200)
+      .json(await profile.getAllUserInfo(req.session.passport.user));
   } catch (err) {
     return res.status(500).json(err.stack);
   }
 });
 
+// File Upload Route handling
+// TODO Set these as environment variables
+AWS.config.update({
+  accessKeyId: 'AKIAIGV3I6CABFE4JKYQ',
+  secretAccessKey: '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',
+  subregion: 'us-west-1',
+});
+const s3 = new AWS.S3();
+
+const upload = multer({
+  storage: multerS3({
+    s3,
+    limits: { fileSize: 524280 },
+    bucket: 'airbnbcasa',
+    key(req, file, cb) {
+      cb(null, Date.now().toString());
+    },
+    ACL: 'public-read',
+  }),
+});
+
+router.post(
+  '/api/user/profilepictureupload',
+  upload.single('profilePicture'),
+  async (req, res) => {
+    try {
+      if (!req.session.passport) {
+        return res.sendStatus(401);
+      }
+      await profile.uploadPicture(req.session.passport.user, req.file.location);
+      return res.sendStatus(200);
+    } catch (err) {
+      return res.status(500).json(err.stack);
+    }
+  },
+);
+
+// update profile
+router.post('/api/updateprofile', async (req, res) => {
+  try {
+    if (!req.session.passport) {
+      return res.sendStatus(401);
+    }
+    await profile.updateProfile(
+      req.session.passport.user,
+      req.body.displayName,
+      req.body.location,
+      req.body.tagline,
+      req.body.bio,
+    );
+    return res.sendStatus(200);
+  } catch (err) {
+    return res.status(500).json(err.stack);
+  }
+});
 
 /*
   React Router
@@ -172,17 +253,21 @@ router.get('/api/user/profile', async (req, res) => {
 
 router.get(
   '/login',
-  (req, res) => (req.session.passport ? res.redirect('/') : reactRoute(req, res)),
+  (req, res) =>
+    req.session.passport ? res.redirect('/') : reactRoute(req, res),
 );
 router.get(
   '/signup',
-  (req, res) => (req.session.passport ? res.redirect('/') : reactRoute(req, res)),
+  (req, res) =>
+    req.session.passport ? res.redirect('/') : reactRoute(req, res),
 );
 
-router.get('/logoff', (req, res) => req.session.destroy(() => {
-  res.clearCookie('connect.sid');
-  res.redirect('/');
-}));
+router.get('/logoff', (req, res) =>
+  req.session.destroy(() => {
+    res.clearCookie('connect.sid');
+    res.redirect('/');
+  }),
+);
 router.get('/listings*', reactRoute);
 router.get('/bookings*', protectedReactRoute);
 router.get('/host', protectedReactRoute);
